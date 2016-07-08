dc0b4f1409cdbc3763343de143457b3104acb0ff
Gabe Dickinson <gabe.dickinson@gmail.com>
2016-07-07 22:14:08 -0500

    initial

---
+++ main.js
@@ -0,0 +1,7 @@
+var suits = ['Clubs', 'Spades', 'Diamonds', 'Hearts'];
+var values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+function newCard(suit, value) {
+  this.suit = suit;
+  this.value = value;
+}
