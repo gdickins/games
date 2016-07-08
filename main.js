1147c7fc45810fbf1082206b9d9d891c976957af
Gabe Dickinson <gabe.dickinson@gmail.com>
2016-07-07 23:03:38 -0500

    finished deck and rolldice

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 var suits = ['Clubs', 'Spades', 'Diamonds', 'Hearts'];
 var values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-function newCard(suit, value) {
+function makeCard(suit, value) {
   this.suit = suit;
   this.value = value;
+}
+function makeDeck() {
+  var deck = [];
+  for (var i = 0; i < suits.length; i++) {
+        for (var j = 0; j < values.length; j++) {
+            deck.push(new makeCard(values[j], suits[i]));
+        }
+    }
+    return deck;
+}
+
+function rollDice() {
+  this.value = Math.ceil((6*Math.random()));
+}
+
+function rollThem() {
+  var results = [];
+  for(var i = 0; i < 1000; i++) {
+    results.push(new rollDice());
+  }
+  return results;
 }
